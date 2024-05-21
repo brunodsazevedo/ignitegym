d91f60d32655c0aa3efcf2c1060d20cb72d35cab
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2024-05-21 01:20:12 -0300

    fix: feito melhora de loading em pagina de histórico

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -4,6 +4,7 @@ import { Heading, VStack, SectionList, Text, useToast } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
+import { Loading } from '@components/Loading';
 
 import { AppError } from '@utils/AppError';
 
@@ -43,27 +44,29 @@ export function History() {
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
 
-      <SectionList
-        sections={exercises}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <HistoryCard data={item} />
-        )}
-        renderSectionHeader={({ section }) => (
-          <Heading fontFamily="heading" color="gray.200" fontSize="md" mt={10} mb={3}>
-            {section.title}
-          </Heading>
-        )}
-        px={8}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
-        ListEmptyComponent={() => (
-          <Text color="gray.100" textAlign="center">
-            Não há exercícios registrados ainda. {'\n'}
-            Vamos fazer exercícios hoje?
-          </Text>
-        )}
-      />
+      {isLoading ? <Loading /> : (
+        <SectionList
+          sections={exercises}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <HistoryCard data={item} />
+          )}
+          renderSectionHeader={({ section }) => (
+            <Heading fontFamily="heading" color="gray.200" fontSize="md" mt={10} mb={3}>
+              {section.title}
+            </Heading>
+          )}
+          px={8}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+          ListEmptyComponent={() => (
+            <Text color="gray.100" textAlign="center">
+              Não há exercícios registrados ainda. {'\n'}
+              Vamos fazer exercícios hoje?
+            </Text>
+          )}
+        />
+      )}
     </VStack>
   );
 }
